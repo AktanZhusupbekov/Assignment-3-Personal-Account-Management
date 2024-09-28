5d0eacf030a9eae8460dad990ecb9c09a3105d31
AktanZhusupbekov <150648923+AktanZhusupbekov@users.noreply.github.com>
2024-09-28 14:11:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+/**
+ * The main entry point for the program.
+ */
+public class Main {
 
+    public static void main(String[] args) {
+        // Create a new PersonalAccount object
+        PersonalAccount account = new PersonalAccount(230106010, "Aktan Zhusupbekov");
+
+        // Make some transactions
+        account.deposit(4123);
+        account.withdraw(800);
+        account.deposit(193);
+
+        // Print the account details
+        System.out.println("Account Holder: " + account.getAccountHolder());
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Balance: $" + account.getBalance());
+
+        // Print the transaction history
+        account.printTransactionHistory();
+    }
+}
